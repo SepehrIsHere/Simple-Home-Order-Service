62af7cf88ef9bc34d914f8bbc1741056b087f371
Sepehr <sepehristaken@gmail.com>
2024-11-28 00:57:47 -0800

    add essentials

---
+++ src/main/java/org/practice/simplehomeorderservice/SimplehomeorderserviceApplication.java
@@ -0,0 +1,13 @@
+package org.practice.simplehomeorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplehomeorderserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplehomeorderserviceApplication.class, args);
+    }
+
+}
